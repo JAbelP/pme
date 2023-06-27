4ca95cb45f3d84ab1f9dc565a4b283493f73ec85
Abel Pinales <abelpinales97@gmail.com>
2023-06-27 02:42:03 -0400

    Ready To Show Hal

--- app/components/contactUsComponents/ContactUs.jsx
+++ app/components/contactUsComponents/ContactUs.jsx
@@ -65,7 +65,7 @@ export const ContactUs = () => {
   return (
     <div className='flex flex-col justify-center items-center gap-y-1'>
       <select
-        className="text-black px-3 py-2 w-60 lg:w-[300px] h-10 mb-3 rounded-sm"
+        className="text-black px-3 py-2 w-60 lg:w-[450px] h-10 mb-3 rounded-sm"
         value={selectedOption}
         onChange={handleDropDownChange}
       >
@@ -76,7 +76,7 @@ export const ContactUs = () => {
         ))}
       </select>
       <input
-        className={`text-black px-3 py-2 lg:w-[300px] h-12 mb-${nameWarning ? 1 : 3} rounded-sm`}
+        className={`text-black px-3 py-2 lg:w-[450px] h-12 mb-${nameWarning ? 1 : 3} rounded-sm`}
         type="text"
         placeholder="Full Name"
         onChange={handleFullNameChange}
@@ -87,7 +87,7 @@ export const ContactUs = () => {
         <p className="text-red-500 text-xs">Please fill in the Full Name field.</p>
       )}
       <input
-        className={`text-black px-3 py-2 lg:w-[300px] h-10 mb-${emailWarning ? 1 : 3} rounded-sm`}
+        className={`text-black px-3 py-2 lg:w-[450px] h-10 mb-${emailWarning ? 1 : 3} rounded-sm`}
         type="text"
         placeholder="Email"
         onChange={handleEmailChange}
@@ -98,7 +98,7 @@ export const ContactUs = () => {
         <p className="text-red-500 text-xs">Please fill in the Email field.</p>
       )}
       <input
-        className={`text-black px-3 py-2 lg:w-[300px] h-10 mb-${phoneWarning ? 1 : 3} rounded-sm`}
+        className={`text-black px-3 py-2 lg:w-[450px] h-10 mb-${phoneWarning ? 1 : 3} rounded-sm`}
         type="text"
         placeholder="Phone Number"
         onChange={handlePhoneChange}
@@ -109,7 +109,7 @@ export const ContactUs = () => {
         <p className="text-red-500 text-xs">Please fill in the Phone Number field.</p>
       )}
       <button
-        className="flex justify-center items-center w-64 bg-yellow-300 text-black px-5 py-4 h-[57px] lg:w-[300px]"
+        className="flex justify-center items-center w-64 bg-yellow-300 text-black px-5 py-4 h-[57px] lg:w-[450px]"
         onClick={handleSubmit}
       >
         Get In Touch Now!
