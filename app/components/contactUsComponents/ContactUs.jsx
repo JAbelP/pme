ae31701e51014a7dab06db87abf10562fadb3239
Abel Pinales <abelpinales97@gmail.com>
2023-06-26 23:39:43 -0400

    Trial

--- app/components/contactUsComponents/ContactUs.jsx
+++ app/components/contactUsComponents/ContactUs.jsx
@@ -1,60 +1,119 @@
 'use client';
-import React ,{useState} from "react";
+import React, { useState, useEffect } from "react";
+import { usePathname } from 'next/navigation';
 
+export const ContactUs = () => {
+  const pathname = usePathname();
 
-export const ContactUs = () =>{
-    const options = [
-        'General Installation',
-        'Electrical Repair',
-        'Generator Maintenance',
-        'Commercial Electrical Service',
-        'Electrical Panel Replacement/Installation',
-        'Other',
-      ];
+  const options = [
+    'General Installation',
+    'Electrical Repair',
+    'Generator Maintenance',
+    'Commercial Electrical Service',
+    'Electrical Panel Replacement/Installation',
+    'Other',
+  ];
 
-        // WHAT THE USER HAS SELECTED ON THE SERVICE
-  const [selectedOption, setSelectedOption] = useState('yes');
-  // FULL NAME OF THE USER
-  const [fullName, setFullName] = useState("Abel");
-  // EMAIL OF THE USER
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
-  // PHONE NUMBER OF THE USER
-  const [ phone, setPhone ] = useState('');
-  // GETS THE CURRENT PAGE (URL)
-  const [ page, setPage ] = useState(0);
-  const [ nameWarning, setNameWarning ] = useState(false);
-  const [ emailWarning, setEmailWarning ] = useState(false);
-  const [ phoneWarning, setPhoneWarning ] = useState(false);
+  const [phone, setPhone] = useState('');
+  const [nameWarning, setNameWarning] = useState(false);
+  const [emailWarning, setEmailWarning] = useState(false);
+  const [phoneWarning, setPhoneWarning] = useState(false);
+
+  useEffect(() => {
+    if (pathname === 'generators/installation') {
+      setSelectedOption(options[0]);
+    }
+    if (pathname === 'generators/maintenance') {
+      setSelectedOption(options[2]);
+    }
+  }, [pathname]);
 
   const handleDropDownChange = (e) => {
     setSelectedOption(e.target.value);
   };
-  
-  // Handles the change of the Name
+
   const handleFullNameChange = (e) => {
     setFullName(e.target.value);
     setNameWarning(false);
   };
 
-  // Handle the change of the Email
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
     setEmailWarning(false);
   };
 
-  //handles the change of the Phone Number
   const handlePhoneChange = (e) => {
     setPhone(e.target.value);
     setPhoneWarning(false);
   };
 
-  const increasePage = () => {
-    setPage(1);
-}
-    return (
-        <div className='flex flex-col justify-center items-center gap-y-1'>
+  const handleSubmit = () => {
+    if (fullName === '') {
+      setNameWarning(true);
+    }
+    if (email === '') {
+      setEmailWarning(true);
+    }
+    if (phone === '') {
+      setPhoneWarning(true);
+    }
+  };
 
-
-        </div>
-      );
-}
+  return (
+    <div className='flex flex-col justify-center items-center gap-y-1'>
+      <select
+        className="text-black px-3 py-2 w-60 lg:w-[300px] h-10 mb-3 rounded-sm"
+        value={selectedOption}
+        onChange={handleDropDownChange}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <input
+        className={`text-black px-3 py-2 lg:w-[300px] h-12 mb-${nameWarning ? 1 : 3} rounded-sm`}
+        type="text"
+        placeholder="Full Name"
+        onChange={handleFullNameChange}
+        value={fullName}
+        onClick={() => setNameWarning(false)}
+      />
+      {nameWarning && (
+        <p className="text-red-500 text-xs">Please fill in the Full Name field.</p>
+      )}
+      <input
+        className={`text-black px-3 py-2 lg:w-[300px] h-10 mb-${emailWarning ? 1 : 3} rounded-sm`}
+        type="text"
+        placeholder="Email"
+        onChange={handleEmailChange}
+        value={email}
+        onClick={() => setEmailWarning(false)}
+      />
+      {emailWarning && (
+        <p className="text-red-500 text-xs">Please fill in the Email field.</p>
+      )}
+      <input
+        className={`text-black px-3 py-2 lg:w-[300px] h-10 mb-${phoneWarning ? 1 : 3} rounded-sm`}
+        type="text"
+        placeholder="Phone Number"
+        onChange={handlePhoneChange}
+        value={phone}
+        onClick={() => setPhoneWarning(false)}
+      />
+      {phoneWarning && (
+        <p className="text-red-500 text-xs">Please fill in the Phone Number field.</p>
+      )}
+      <button
+        className="flex justify-center items-center w-64 bg-yellow-300 text-black px-5 py-4 h-[57px] lg:w-[300px]"
+        onClick={handleSubmit}
+      >
+        Get In Touch Now!
+      </button>
+    </div>
+  );
+};
--- app/generators/installation/page.jsx
+++ app/generators/installation/page.jsx
@@ -15,7 +15,7 @@ function page() {
       <Headers/>
       
        <EyeCatch/>
-        <div className='bg-white text-black relative mt-10 flex flex-col text-center sm:flex-row  pb-3'>
+        <div className='bg-white text-black relative mt-10 flex flex-col text-center sm:flex-row lg:px-40 pb-3'>
         {/* split this please */}
           <div className='lg:w-1/4 md:2/3'>
           {/* Right hand side */}
@@ -40,7 +40,7 @@ function page() {
               best one for your family but stay in your budget also. Reach out to a member of our generator
                team today for a free, no obligation generator consultation today!</p>
             <MidCatchChatYellow/>
-            <p className='mt-3 text-2xl text-left'>Expert Generator Installation</p>
+            <p className='mt-3 text-2xl text-left pl-4'>Expert Generator Installation</p>
             <p className='p-6 text-left'>
             Our expert team is ready to handle and wire your new generator directly
              into your service panel or transfer switch. Doing this will make 
@@ -50,6 +50,14 @@ function page() {
               hazards, so make sure you have your generator installed by a 
               certified professional every time.
             </p>
+            <p className='mt-3 text-2xl text-left pl-4'>Expert Generator Installation</p>
+            <p className='p-6 text-left'>
+              Have The Experts Install Your New Generator!
+              At PowerMaster Electric,  your safety and  satisfaction are our highest priority .
+              We are a full-service Generac generator installer. Please consider Powermaster Electric for your generator 
+              purchase, installation, maintenance and/or repair.
+            </p>
+
             <MidCatchChatRed/>
           </div>
           {/* LEFT HAND SIDE  */}
