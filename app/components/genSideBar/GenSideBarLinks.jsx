7e6ffedf9643947f2f5b597d7fa53f3acf89aea6
Abel Pinales Desktop <abelpinales97@gmail.com>
2023-07-10 16:23:31 -0400

    Blog Body Mostly Done

--- app/components/genSideBar/GenSideBarLinks.jsx
+++ app/components/genSideBar/GenSideBarLinks.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 export const GenSideBarLinks = () => {
   return (
-    <div className='bg-orange-400 mt-5 pt-4 w-5/6 mb-3 mx-auto border-y-[25px] lg:w-72 border-black text-left pl-4 py-12'>
-        <div className='text-2xl '>
+    <div className='bg-orange-400 mt-5 pt-4 w-6/7 mb-3 mx-auto border-y-[25px] lg:w-72 border-black text-left pl-4 py-12'>
+        <div className=' text-black text-3xl font-bold leading-9'>
             Generators
         </div>
         <div className='capitalize pl-4'>
-            <p className='hover:text-white py-2'> Generator Installation</p>
-            <p className='hover:text-white py-2'> Generator Maintenance</p>
-            <p className='hover:text-white py-2'> Generator Repair</p>
-            <p className='hover:text-white py-2'> Generator Types & Pricing</p>
-            <p className='hover:text-white py-2'> Whole House Generators</p>
-            <p className='hover:text-white py-2'> Portal Generators</p>
-            <p className='hover:text-white py-2'> Generator Financing</p>
+            <p className='text-black text-base font-bold leading-relaxed hover:text-white py-2'> Generator Installation</p>
+            <p className='text-black text-base font-bold leading-relaxed hover:text-white py-2'> Generator Maintenance</p>
+            <p className='text-black text-base font-bold leading-relaxed hover:text-white py-2'> Generator Repair</p>
+            <p className='text-black text-base font-bold leading-relaxed hover:text-white py-2'> Generator Types & Pricing</p>
+            <p className='text-black text-base font-bold leading-relaxed hover:text-white py-2'> Whole House Generators</p>
+            <p className='text-black text-base font-bold leading-relaxed hover:text-white py-2'> Portal Generators</p>
+            <p className='text-black text-base font-bold leading-relaxed hover:text-white py-2'> Generator Financing</p>
             
 
         </div>
