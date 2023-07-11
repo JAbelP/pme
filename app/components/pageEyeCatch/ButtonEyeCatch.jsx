6199af211e6a0ec5c5a067319df9ca85920beabb
Abel Pinales Desktop <abelpinales97@gmail.com>
2023-07-11 03:05:42 -0400

    changed from button

--- app/components/pageEyeCatch/ButtonEyeCatch.jsx
+++ app/components/pageEyeCatch/ButtonEyeCatch.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 export const ButtonEyeCatch = (props) => {
   return (
     <div className='flex flex-col items-center justify-center text-center '>
-      <button className='bg-yellow-300 h-32 w-32 text-black mt-7 rounded-full mb-3'>
+      <div className='bg-yellow-300 h-32 w-32 text-black mt-7 rounded-full mb-3'>
         <div className='flex items-center justify-center h-full'>
           <Image src={props.image} alt={props.alt} width={57} height={57} />
         </div>
-      </button>
+      </div>
       <div className='text-center text-white text-lg font-normal uppercase leading-loose'>
         <h5>{props.text}</h5>
       </div>
